01f2af53d31157d54c98f07eeba8669cccf8844b
JaredGB1 <mjaredgb@gmail.com>
2023-11-26 20:46:38 -0600

    Discover page done

---
+++ chamber/scripts/lastVisit.js
@@ -0,0 +1,28 @@
+const visitsDisplay = document.querySelector("#lastVisits");
+const msToDays = 84600000;
+const today=Date.now();
+const lastVisit= localStorage.getItem('lastVisit') || Date.now();
+const numberOfDaysLastVisit= lastVisit/msToDays;
+const numberOfDaysToday=today/msToDays;
+const days=numberOfDaysToday-numberOfDaysLastVisit;
+const roundedDays=Math.round(days);
+if(days==0)
+{
+    visitsDisplay.textContent="Welcome! Let us know if you have any questions.";
+}
+else if(days<1)
+{
+    visitsDisplay.textContent="Back so soon! Awesome!";
+}
+else
+{
+    if(days<2)
+    {
+        visitsDisplay.textContent="You last visited 1 day ago";
+    }
+    else
+    {
+        visitsDisplay.textContent="You last visited "+roundedDays+" days ago";
+    }
+}
+localStorage.setItem('lastVisit',Date.now());
