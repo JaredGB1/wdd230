7ef68e2238f55bd91cb37936d76a6bc95aedeedc
JaredGB1 <mjaredgb@gmail.com>
2023-12-09 20:50:39 -0600

    changes

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,4 +1,5 @@
 const currentYear= new Date().getFullYear();
 const lastModified=document.lastModified;
+const date=new Date();
 document.querySelector('#currentYear').textContent=currentYear;
 document.querySelector('#lastModified').textContent=`Last modified: ${lastModified}`;
