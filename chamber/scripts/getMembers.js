1964b62567b6a80ff4a2d027dd2a76d1eed01b01
JaredGB1 <mjaredgb@gmail.com>
2023-12-12 00:13:27 -0600

    Changes JSON

---
+++ chamber/scripts/getMembers.js
@@ -0,0 +1,34 @@
+const linksURL="https://jaredgb1.github.io/wdd230/chamber/data/members.json";
+const baseURL="https://jaredgb1.github.io/wdd230";
+const memberGrid = document.querySelector('.membersGrid');
+async function getMembers ()
+{
+    const response=await fetch(linksURL);
+    const data=await response.json();
+    displayLinks(data.members);
+}
+getMembers();
+
+const displayLinks= (data) =>
+{   
+    data.forEach(member => {
+        let section=document.createElement('section');
+        let img = document.createElement('img');
+        let name =document.createElement('h3');
+        let address=document.createElement('p');
+        let phone=document.createElement('p');
+        let website=document.createElement('a');
+        let membership=document.createElement('p');
+        let description=document.createElement('p');
+        img.setAttribute("src", member.icon);
+
+        section.appendChild(img);
+        section.appendChild(name);
+        section.appendChild(address);
+        section.appendChild(phone);
+        section.appendChild(website);
+        section.appendChild(membership);
+        section.appendChild(description);
+        memberGrid.appendChild(section);
+    });
+}
